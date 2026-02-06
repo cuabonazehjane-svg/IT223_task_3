ce8c8a7b20e8e1e5023d4d5619f6a2843b996a7b
cuabonazehjane-svg <cuabonazehjane@gmail.com>
2026-02-06 15:46:42 +0800

    Add HTML structure to bin.php for BIN function display

--- AdvanceFunctions/bin.php
+++ AdvanceFunctions/bin.php
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BIN</title>
+</head>
+<body>
+    <?php
+include "../config/db.php";
 
+$result = $conn->query("SELECT BIN(15) AS result");
+$row = $result->fetch_assoc();
+
+echo "<h3>BIN()</h3>";
+echo $row['result'];
+?>
+</body>
+</html>
